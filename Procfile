1e76497bd7d3c2dc875eb54c934df239a80ac335
mahmudurmahid <m.mahid@icloud.com>
2025-03-03 14:48:57 +0000

    heroku deployment and initial set up

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn developer_resume.wsgi
---
+++ cv_feedback_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CvFeedbackAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cv_feedback_app'
